f032336da1620f541d6ece8171d64fa5f8fc18f3
jefripunza <jefripunza@gmail.com>
2021-12-25 18:05:43 +0700

    fix Telegram

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
 // ============ Test Area ============
+
+const Telegram = require("./bot/Telegram")
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_TOKEN;
+
+const user_target = "1228921740"
+
 module.exports = ({ app, webserver }) => {
     async function run() {
-
+        const bot = new Telegram(token)
+        bot.listenMessage(async listen => {
+            console.log({ listen });
+        })
+        // setInterval(() => {
+        //     bot.sendMessage(user_target, "hai")
+        // }, 1000);
     }
     run()
 }
